c3193ec3934cc2dbdaa0e0310f9f1a0a71a0f232
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-02-16 15:56:36 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,40 +1,40 @@
 variable "name" {
   description = "Prefix (eg. abc) of the instance profile (abcProfile) and role (abcRole) names"
-  type        = "string"
+  type        = string
 }
 
 variable "policy_jsons" {
   description = "Valid JSON policies for the role"
-  type        = "list"
+  type        = list
   default     = []
 }
 
 variable "policy_arns" {
   description = "ARNs of IAM policies for the role"
-  type        = "list"
+  type        = list
   default     = []
 }
 
 variable "attach_ssm_policy" {
   description = "Toggles attachment of the AmazonSSMManagedInstanceCore policy to allow usage of AWS SSM"
-  type        = "string"
+  type        = string
   default     = false
 }
 
 variable "attach_cwagent_policy" {
   description = "Toggles attachment of the CloudWatchAgentServerPolicy policy to allow usage of CloudWatch agent"
-  type        = "string"
+  type        = string
   default     = false
 }
 
 variable "path" {
   description = "Path for the instance profile, role and user-managed policy (if any)"
-  type        = "string"
+  type        = string
   default     = "/"
 }
 
 variable "tags" {
   description = "Tags that should be assigned to the created resources whenever possible"
-  type        = "map"
+  type        = map
   default     = {}
 }
