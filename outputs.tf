a36097e0b27af05ed94a3db227808d9b218882b0
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-04-05 15:48:36 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "profile_arn" {
   description = "Instance profile name"
   value       = "${aws_iam_instance_profile.main.arn}"
 }
+
+output "role_arn" {
+  description = "Instance role name"
+  value       = join("", aws_iam_role.main.*.arn)
+ 
+}
