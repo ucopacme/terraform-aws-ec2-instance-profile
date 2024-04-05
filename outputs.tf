a4d8395c1a1a1bb7f0092a232469e0d64ef3a432
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-04-05 15:15:40 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "profile_name" {
   description = "Instance profile name"
   value       = "${aws_iam_instance_profile.main.name}"
 }
+
+output "profile_arn" {
+  description = "Instance profile name"
+  value       = "${aws_iam_instance_profile.main.arn}"
+}
