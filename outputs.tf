af26bd3d3724016d4864736666f7bd49b0c5d63b
George Holbert <gholbert@ucop.edu>
2024-11-12 13:14:38 -0800

    minor cosmetics, outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,14 @@
 output "profile_name" {
   description = "Instance profile name"
-  value       = "${aws_iam_instance_profile.main.name}"
+  value       = aws_iam_instance_profile.main.name
 }
 
 output "profile_arn" {
   description = "Instance profile name"
-  value       = "${aws_iam_instance_profile.main.arn}"
+  value       = aws_iam_instance_profile.main.arn
 }
 
 output "role_arn" {
   description = "Instance role name"
   value       = join("", aws_iam_role.main.*.arn)
- 
 }
